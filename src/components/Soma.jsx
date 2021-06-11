9b39109fce8115643ff52a1b252ae9222d8b73e0
hst.paulo <hst.paulo@gmail.com>
2021-06-11 19:57:01 -0300

    desafio de implementacao para os componentes soma e sorteio

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Card from './Card'
 
-export default props => {
+const Soma = props => {
+    
+    const { min, max } = props
 
     return(
         <Card title="Soma dos números" green>
             <div>
                 <span>
                     <strong>Resultado: </strong>
-                    <span>3</span>
+                    <span>{ min + max }</span>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state){
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Soma)
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Card from './Card'
 
-export default props => {
+const Sorteio = props => {
+
+    const { min, max } = props
 
     return(
         <Card title="Sorteio de um número" purple>
             <div>
                 <span>
                     <strong>Resultado: </strong>
-                    <span>5</span>
+                    <span>{ parseInt(Math.random() * (max - min) + min) }</span>
                 </span>
             </div>
         </Card>
     )
 }
+
+const mapStateToProps = state => {
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio)
