3d65fd39b9ee1eeb532b54390090d51f80897018
hst.paulo <hst.paulo@gmail.com>
2021-06-11 20:45:24 -0300

    refatorando reducer

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Card from './Card'
-import { alterarNumeroMaximo, alterarNumeroMinimo } from '../store/actions/numeros'
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numeros'
 
 import './Intervalo.css'
 
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,37 +1,8 @@
 import { createStore, combineReducers } from 'redux'
+import numerosReducer from './reducers/numeros'
 
 const reducers = combineReducers({
-    numeros: function(state, action){
-
-        switch(action.type){
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state, 
-                    min: action.payload
-                }
-            case 'NUM_MAX_ALTERADO':
-                return{
-                    ...state,
-                    max: action.payload
-                }
-            default: {
-                return {
-                    min: 10,
-                    max: 55
-                }
-            }
-        }
-        
-    },
-    nomes: function(state, action){
-        console.log('Reducer nomes .. ')
-        console.log(state, ' ', action)
-        return[
-            'Paulo',
-            'Henrique',
-            'Teixeira'
-        ]
-    }
+    numeros: numerosReducer
 })
 
 function storeConfig(){
